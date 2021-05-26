f8c3209c157ae070fb303513b572cc26d7b6f27c
ts2905 <84340990+ts2905@users.noreply.github.com>
2021-05-25 20:18:42 -0400

    Added user choice

--- game.py
+++ game.py
@@ -2,6 +2,9 @@
 
 print("Rock, Paper, Scissors, Shoot!")
 
+user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
+
+#print(user_choice)
+print("USER CHOICE: ",user_choice)
 
 
-print(10)
