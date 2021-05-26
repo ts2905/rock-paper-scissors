5881b8e690849fee47898e83c5da61ac6bd2824c
ts2905 <84340990+ts2905@users.noreply.github.com>
2021-05-25 20:38:06 -0400

    Added user_input validation

--- game.py
+++ game.py
@@ -7,4 +7,11 @@ user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 #print(user_choice)
 print("USER CHOICE: ",user_choice)
 
+#if user_choice == "rock" or "paper" or "scissors":
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
+    print("VALID. KEEP GOING")
+else:
+    print("OOPS, invalid input. Please try again.")
+    exit()
 
+print("THIS IS THE END OF THE GAME. PLEASE PLAY AGAIN.")
